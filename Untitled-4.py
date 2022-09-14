b90b7192ad377a9788cc32c939a458628692c510
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2022-09-14 20:51:14 +0300

    Update Untitled-4.py

--- Untitled-4.py
+++ Untitled-4.py
@@ -2,8 +2,8 @@ def creare_lista():
     n = int(input('Numarul de elemente: '))
     lista_locala = []
     for i in range(n):
-        element = input(f'Elementul {i} este: ')
-        if isinstance(element, element) == True:
+        element = eval(input(f'Elementul {i} este: '))
+        if isinstance(element, int) == True:
             lista_locala.append(element)
     return lista_locala
 
